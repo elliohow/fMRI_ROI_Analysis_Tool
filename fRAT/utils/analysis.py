cfbcf42b9c6f6834f774103744d0bf0afbb024c2
Elliot Howley <elliohow@hotmail.com>
2020-12-17 17:11:35 +0000

    * Made grey matter segmentation more efficient

--- fRAT/utils/analysis.py
+++ fRAT/utils/analysis.py
@@ -371,9 +371,7 @@ class Analysis:
 
         elif config.grey_matter_segment == 'fslfast':
             # If voxel has a value below the threshold then set to 1
-            for counter, voxel in enumerate(segment_brain):  # TODO change this to be more efficient
-                if voxel < config.fslfast_min_prob:
-                    idxCSF_or_WM[counter] = 1
+            idxCSF_or_WM = (segment_brain < config.fslfast_min_prob).astype(int)
 
         return idxCSF_or_WM
 
